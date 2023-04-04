54e174c600b88d9142a9de0ceb3ab8e67807746b
yazed <s201730270@kfupm.edu.sa>
2023-04-04 06:50:11 +0300

    - Finished date picker with full validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="grid place-items-center h-full bg-Off-White">
-        <div className='shadow-lg p-6 bg-White rounded-3xl rounded-br-[4rem]'>
+        <div className='shadow-lg p-6 lg:p-16 bg-White rounded-3xl rounded-br-[4rem] min-w-0 max-w-[90rem] mx-3'>
             <DatePicker />
         </div>
     </div>
--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,29 +1,122 @@
 import {useState} from "react";
 
 export const DatePicker = () => {
-    const [dayValid, setDayValid] = useState(false);
-    const [monthValid, setMonthValid] = useState(false);
-    const [yearValid, setYearValid] = useState(false)
+   const [dayErrorMessage, setDayErrorMessage] = useState(null);
+    const [monthErrorMessage, setMonthErrorMessage] = useState(null);
+    const [yearErrorMessage, setYearErrorMessage] = useState(null);
+    const [dateErrorMessage, setDateErrorMessage] = useState(null);
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
 
     // function validate
+    function checkDay(e) {
+        console.log('e.target.value', e.target.value);
+       if (e.target.value === '') {
+            setDayErrorMessage('This field is required');
+            setDay('')
+        } else if (parseInt(e.target.value) > 31 || parseInt(e.target.value) < 1 || !Number.isInteger(parseInt(e.target.value))) {
+            setDayErrorMessage('Must be valid day');
+            setTimeout(() => {
+                setDayErrorMessage(null);
+            }
+            , 3000)
+        } else {
+            setDayErrorMessage(null);
+            setDay(e.target.value);
+            validateDate(e.target.value, month, year);
+        }
+    }
+
+    function checkMonth(e) {
+        if (e.target.value === '') {
+            setMonthErrorMessage('This field is required');
+            setMonth('')
+        } else if (parseInt(e.target.value) > 12 || parseInt(e.target.value) < 1 || !Number.isInteger(parseInt(e.target.value))) {
+            setMonthErrorMessage('Must be valid month');
+            setTimeout(() => {
+                setMonthErrorMessage(null);
+            }
+            , 3000)
+        } else {
+            setMonthErrorMessage(null);
+            setMonth(e.target.value);
+            validateDate(day, e.target.value, year);
+        }
+    }
+
+    function checkYear(e) {
+        if (e.target.value === '') {
+            setYearErrorMessage('This field is required');
+            setYear('')
+        } else if (parseInt(e.target.value) > new Date().getFullYear() || !Number.isInteger(parseInt(e.target.value))) {
+            setYearErrorMessage('Must be valid year');
+            setTimeout(() => {
+                setYearErrorMessage(null);
+            }
+            , 3000)
+        } else {
+            setYearErrorMessage(null);
+            setYear(e.target.value);
+            validateDate(day, month, e.target.value);
+        }
+    }
+
+    function validateDate(day, month, year) {
+        if (day === '' || month === '' || year === '') {
+           return;
+        }
+        const date = new Date(year, month - 1, day);
+        const getDays = (year, month) => {
+            return new Date(year, month, 0).getDate();
+        };
+        const daysInMonth = getDays(year, month);
+        if (daysInMonth < parseInt(day)) {
+            setDateErrorMessage('Must be valid date');
+            return;
+        }
+        if (date > new Date()) {
+            setDateErrorMessage('Must be valid date');
+            return;
+        }
+        setDateErrorMessage(null);
+
+    }
 
     return <section id='date-picker-section'
-                    className='flex justify-start text-Smokey-Grey font-Poppins font-bold text-xs tracking-[0.2rem]'>
-        <label htmlFor="dayInput" className='flex flex-col grow-0'>DAY
+                    className='grid grid-cols-3 gap-3 lg:gap-6 text-Smokey-Grey font-Poppins font-extrabold text-xs tracking-[0.2rem] w-fit lg:mr-56'>
+        <div className='flex flex-col'>
+        <label htmlFor="dayInput">DAY</label>
             <input
-                        className='focus:outline-none focus:border-Purple max-w-[8rem] mt-2 text-xl placeholder-Smokey-Grey text-Off-Black px-4 py-2 border border-Light-Grey rounded'
-                        type="text" pattern='([0-2]\d|3[0-1])' placeholder='DD'/>
-        </label>
-        <label htmlFor="monthInput" className='ml-3 flex flex-col'>MONTH
+                value={day}
+                onChange={checkDay}
+                name='dayInput'
+                className='min-w-0 w-full max-w-[8rem] focus:outline-none focus:border-Purple mt-2 text-xl lg:text-2xl placeholder-Smokey-Grey
+                text-Off-Black py-3 px-4 border border-Light-Grey rounded cursor caret-Purple'
+                type="text" pattern='([0-2]\d|3[0-1])' placeholder='DD'/>
+            {dayErrorMessage? <ErrorMessage errorMessage={dayErrorMessage}/> : ''}
+            {dateErrorMessage? <ErrorMessage errorMessage={dateErrorMessage}/> : ''}
+        </div>
+        <div className='flex flex-col'>
+        <label htmlFor="monthInput">MONTH</label>
             <input
-                className='focus:outline-none focus:border-Purple max-w-[8rem] mt-2 text-xl placeholder-Smokey-Grey text-Off-Black px-4 py-2 border border-Light-Grey rounded'
+                value={month}
+                onChange={checkMonth}
+                name='monthInput'
+                className='min-w-0 w-full max-w-[8rem] focus:outline-none focus:border-Purple mt-2 text-xl lg:text-2xl placeholder-Smokey-Grey text-Off-Black px-4 py-3 border border-Light-Grey rounded caret-Purple'
                 type="text" pattern='(0[1-9]|1[0-2])' placeholder='MM'/>
-        </label>
-        <label htmlFor="yearInput" className='ml-3 flex flex-col'>YEAR
+            {monthErrorMessage? <ErrorMessage errorMessage={monthErrorMessage}/> : ''}
+        </div>
+        <div className='flex flex-col'>
+        <label htmlFor="yearInput">YEAR</label>
             <input
-                className='focus:outline-none focus:border-Purple max-w-[8rem] mt-2 text-xl placeholder-Smokey-Grey text-Off-Black px-4 py-2 border border-Light-Grey rounded'
+                value={year}
+                onChange={checkYear}
+                name='yearInput'
+                className='min-w-0 w-full max-w-[8rem] focus:outline-none focus:border-Purple mt-2 text-xl lg:text-2xl placeholder-Smokey-Grey text-Off-Black px-4 py-3 border border-Light-Grey rounded caret-Purple'
                 type="text" pattern='(1\d{3}|2\d{3})' placeholder='YYYY'/>
-        </label>
+            {yearErrorMessage? <ErrorMessage errorMessage={yearErrorMessage}/> : ''}
+        </div>
     </section>
 }
 
