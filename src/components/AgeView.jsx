b4ee9e652b1ca66a1119717f05a95dec52d048f7
yazed <s201730270@kfupm.edu.sa>
2023-04-04 08:17:42 +0300

    - The app now works perfectly with animations

--- src/components/AgeView.jsx
+++ src/components/AgeView.jsx
@@ -1,9 +1,53 @@
-export const AgeView = ({years = '- -', months = '- -', days = '- -'}) => {
+import {useSpring, animated} from "react-spring";
+
+export const AgeView = ({
+                            years = '- -',
+                            months = '- -',
+                            days = '- -',
+                            newYears = '- -',
+                            newMonths = '- -',
+                            newDays = '- -'
+                        }) => {
+
+    const yearsProps = useSpring({
+        val: typeof newYears === "number" ? newYears : 0,
+        from: {val: typeof years === 'number' ? years : 0}
+    })
+    const monthsProps = useSpring({
+        val: typeof newMonths === "number" ? newMonths : 0,
+        from: {val: typeof months === 'number' ? months : 0}
+    })
+    const daysProps = useSpring({
+        val: typeof newDays === "number" ? newDays : 0,
+        from: {val: typeof days === 'number' ? days : 0}
+    })
+
     return (
         <div className='font-Poppins'>
-            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>{years} <span className='text-Off-Black'>years</span></h1>
-            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>{months} <span className='text-Off-Black'>months</span></h1>
-            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>{days} <span className='text-Off-Black'>days</span></h1>
+            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>
+                {
+                    years === newYears?
+                        '- -'
+                        :
+                        <animated.span>{yearsProps.val.interpolate(val => Math.floor(val))}</animated.span>
+                }
+                <span className='text-Off-Black ml-3'>years</span></h1>
+            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>
+                {
+                    months === newMonths?
+                        '- -'
+                        :
+                        <animated.span>{monthsProps.val.interpolate(val => Math.floor(val))}</animated.span>
+                }
+                <span className='text-Off-Black ml-3'>months</span></h1>
+            <h1 className='text-[3.35rem] leading-[3rem] lg:leading-[5.5rem] lg:text-8xl text-Purple font-extrabold italic'>
+                {
+                    days === newDays?
+                        '- -'
+                        :
+                        <animated.span>{daysProps.val.interpolate(val => Math.floor(val))}</animated.span>
+                }
+                <span className='text-Off-Black ml-3'>days</span></h1>
         </div>
     )
 }
