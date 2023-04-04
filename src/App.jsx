d9dfd85011c8b296f1454258cc5e910b0a8ac75a
yazed <s201730270@kfupm.edu.sa>
2023-04-04 09:13:11 +0300

    - Bottom left corner was not rounded enough for the design so I fixed it

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <div className="grid place-items-center h-full bg-Off-White">
-        <div className='shadow-lg p-6 lg:p-16 bg-White rounded-3xl rounded-br-[4rem] min-w-0 max-w-[90rem] mx-3'>
+        <div className='shadow-lg p-6 lg:p-16 bg-White rounded-3xl rounded-br-[13rem] min-w-0 max-w-[90rem] mx-3'>
             <DatePicker calculateYearsMonthsDays={calculateYearsMonthsDays} />
             <ArrowDivider />
             <AgeView years={years} newYears={newYears} months={months} newMonths={newMonths} days={days} newDays={newDays} />
